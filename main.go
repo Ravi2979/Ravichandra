fdef73ff3f90382ce3f9c94c0a5d4995233c446e
Ravichandra <ravivarma2979@gmail.com>
2025-08-18 22:08:51 -0500

    First Version

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"ravichandra/simplecal"
+)
+
+func main() {
+
+	//add,sub
+	a, b := 10, 7
+	fmt.Println(simplecal.Add(a, b))
+	fmt.Println(simplecal.Subb(a, b))
+
+	//for loop
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	//var char
+	var r int
+	fmt.Println(r)
+
+	//Basic hello world
+	fmt.Println("Hello World")
+
+	//If-else
+	num := 20
+	if num%2 == 0 {
+		fmt.Println("even")
+	} else {
+		fmt.Println("odd")
+	}
+
+	//greater or less
+	rav := 6
+	if rav > 10 {
+		fmt.Println("greater")
+	} else if rav == 10 {
+		fmt.Println("equal")
+	} else {
+		fmt.Println("Less")
+	}
+
+	//switch
+	week := "Sunday"
+	switch week {
+	case "monday":
+		fmt.Println("First weekday")
+	case "Tuesday":
+		fmt.Println("Second weekday")
+	default:
+		fmt.Println("weekend")
+	}
+
+	//defer
+	defer fmt.Println("Varma")
+	fmt.Println("Go")
+	defer fmt.Println("Learn")
+
+	//pointer
+	c := 15
+	p := &c
+	fmt.Println(c)
+	fmt.Println(p)
+	fmt.Println(*p)
+	*p = 40
+	fmt.Println(c)
+
+}
---
+++ simplecal/cal.go
@@ -0,0 +1,9 @@
+package simplecal
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Subb(a, b int) int {
+	return a - b
+}
